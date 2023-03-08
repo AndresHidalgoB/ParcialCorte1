0da3194d4105d034f768b56bdf27f0f8fd6f4d95
Andres Hidalgo <andres_fel.hidalgo@uao.edu.co>
2023-03-08 02:45:47 -0500

    bug del onClick infinito corregido

--- src/components/Total.jsx
+++ src/components/Total.jsx
@@ -2,9 +2,11 @@ import { LotContext } from "../context/LotContext";
 import { useContext, useState , useEffect } from "react";
 export const Total = () => {
   const { lotteries } = useContext(LotContext);
+  const [isDisable,setDisable] = useState(false)
   const [total, setTotal] = useState(0);
   useEffect(() => {
     console.log(lotteries);
+    setDisable(false)
   }, [lotteries]);
 
   const calculateTotal = () => {
@@ -12,11 +14,12 @@ export const Total = () => {
       alert("No se han agregado loterias");
     }
     lotteries.map((lot)=>setTotal(total+parseInt(lot.precio)));
+    setDisable(true)
   };
 
   return (
     <>
-      <button onClick={() => calculateTotal()}>Calcular Total</button>
+      <button onClick={() => calculateTotal()} disabled={isDisable}>Calcular Total</button>
       <h3>${total}</h3>
     </>
   );
